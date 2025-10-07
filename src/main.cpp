995d98e063b6edd74e8273883a87f3f0459b0a37
Om Joshi <joshiom564@gmail.com>
2025-10-07 20:37:31 +0530

    other half map

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
-#include "traffic_map.hpp"
 #include "traffic_light.hpp"
+#include "traffic_map.hpp"
 #include <SFML/Graphics.hpp>
 #include <memory>
 
@@ -25,49 +25,57 @@ int main()
     // traffic_map.add_double_road(east, center);  // Road 3
 
     // Install traffic lights
-    // if (auto intersection = traffic_map.get_junction(center)) 
+    // if (auto intersection = traffic_map.get_junction(center))
     //     intersection->install_light(sf::seconds(5)); // North -> Center
-    
+
     // ------ SVNIT MAP ------
-    const sf::Vector2f gajjar_junction = {700.f, 100.f};
-    const sf::Vector2f bhabha_junction = {200.f, 100.f};
-    const sf::Vector2f nehru_junction = {100.f, 100.f};
-    const sf::Vector2f swami_junction = {100.f, 200.f};
-    const sf::Vector2f swami_entrance_junction = {200.f, 200.f};
-    const sf::Vector2f quarters_junction = {700.f, 375.f};
-    const sf::Vector2f canteen_junction = {200.f, 375.f};
-    const sf::Vector2f temple_junction = {900.f, 300.f};
+    const sf::Vector2f gajjar_junction = { 700.f, 100.f };
+    const sf::Vector2f bhabha_junction = { 200.f, 100.f };
+    const sf::Vector2f nehru_junction = { 100.f, 100.f };
+    const sf::Vector2f swami_junction = { 100.f, 200.f };
+    const sf::Vector2f swami_entrance_junction = { 200.f, 200.f };
+    const sf::Vector2f quarters_junction = { 700.f, 375.f };
+    const sf::Vector2f canteen_junction = { 200.f, 375.f };
+    const sf::Vector2f temple_junction = { 900.f, 300.f };
+    const sf::Vector2f gate1_junction = { 200.f, 700.f };
+    const sf::Vector2f gate2_junction = { 700.f, 700.f };
+    const sf::Vector2f library_junction = { 300.f, 375.f };
+    const sf::Vector2f admin_junction = { 300.f, 500.f };
+    const sf::Vector2f department_junction = { 700.f, 500.f };
 
-   
-
-    traffic_map.add_double_road(bhabha_junction, gajjar_junction, 15.f); 
-    traffic_map.add_double_road(nehru_junction, bhabha_junction, 15.f); 
-    traffic_map.add_double_road(nehru_junction, swami_junction, 15.f); 
-    traffic_map.add_double_road(bhabha_junction, swami_entrance_junction, 15.f); 
+    traffic_map.add_double_road(gate1_junction, { 200.f, 465.f }, 15.f);
+    traffic_map.add_double_road(bhabha_junction, gajjar_junction, 15.f);
+    traffic_map.add_double_road(nehru_junction, bhabha_junction, 15.f);
+    traffic_map.add_double_road(nehru_junction, swami_junction, 15.f);
+    traffic_map.add_double_road(bhabha_junction, swami_entrance_junction, 15.f);
     traffic_map.add_double_road(swami_junction, swami_entrance_junction, 15.f);
-    traffic_map.add_double_road(swami_entrance_junction, {200.f, 300.f}, 15.f); 
-    traffic_map.add_double_road({200.f, 300.f}, {700.f, 300.f}, 15.f); 
-    traffic_map.add_double_road(gajjar_junction, {700.f, 300.f}, 15.f); 
+    traffic_map.add_double_road(swami_entrance_junction, { 200.f, 300.f }, 15.f);
+    traffic_map.add_double_road({ 200.f, 300.f }, { 700.f, 300.f }, 15.f);
+    traffic_map.add_double_road(gajjar_junction, { 700.f, 300.f }, 15.f);
 
-    traffic_map.add_double_road({200.f, 300.f}, canteen_junction, 15.f); 
-    traffic_map.add_double_road(canteen_junction, quarters_junction, 15.f); 
-    traffic_map.add_double_road({700.f, 300.f}, quarters_junction, 15.f); 
+    traffic_map.add_double_road({ 200.f, 300.f }, canteen_junction, 15.f);
+    traffic_map.add_double_road(canteen_junction, quarters_junction, 15.f);
+    traffic_map.add_double_road({ 700.f, 300.f }, quarters_junction, 15.f);
 
-    traffic_map.add_double_road({700.f, 300.f}, temple_junction, 15.f); 
-    traffic_map.add_double_road(quarters_junction, {900.f, 375.f}, 15.f); 
-    traffic_map.add_double_road(gajjar_junction, {900.f, 100.f}, 15.f); 
-    traffic_map.add_double_road({900.f, 100.f}, temple_junction, 15.f); 
-    traffic_map.add_double_road({900.f, 375.f}, temple_junction, 15.f); 
+    traffic_map.add_double_road({ 700.f, 300.f }, temple_junction, 15.f);
+    traffic_map.add_double_road(quarters_junction, { 900.f, 375.f }, 15.f);
+    traffic_map.add_double_road(gajjar_junction, { 900.f, 100.f }, 15.f);
+    traffic_map.add_double_road({ 900.f, 100.f }, temple_junction, 15.f);
+    traffic_map.add_double_road({ 900.f, 375.f }, temple_junction, 15.f);
 
+    traffic_map.add_double_road({ 100.f, 375.f }, canteen_junction, 15.f);
+    traffic_map.add_double_road(canteen_junction, { 200.f, 465.f }, 15.f);
+    traffic_map.add_double_road({ 100.f, 465.f }, { 200.f, 465.f }, 15.f);
+    traffic_map.add_double_road({ 100.f, 465.f }, { 100.f, 375.f }, 15.f);
+    traffic_map.add_double_road({ 100.f, 465.f }, { 100.f, 700.f }, 15.f);
+    traffic_map.add_double_road({ 100.f, 700.f }, gate1_junction, 15.f);
+    traffic_map.add_double_road(gate1_junction, gate2_junction, 15.f);
 
-
-
-
-
-
-
-
-
+    traffic_map.add_double_road(library_junction, admin_junction, 15.f);
+    traffic_map.add_double_road(admin_junction, { 300.f, 700.f }, 15.f);
+    traffic_map.add_double_road(department_junction, gate2_junction, 15.f);
+    traffic_map.add_double_road(department_junction, quarters_junction, 15.f);
+    traffic_map.add_double_road(department_junction, admin_junction, 15.f);
     // Car Spawner Logic
     sf::Clock spawn_timer;
     int max_cars = 5;
@@ -84,8 +92,10 @@ int main()
         }
 
         // Spawn cars on the north and west roads
-        if (spawned_count < max_cars && spawn_timer.getElapsedTime().asSeconds() > 1.5f) {
-            if (auto road = traffic_map.get_double_road(0)) { // North road
+        if (spawned_count < max_cars && spawn_timer.getElapsedTime().asSeconds() > 1.5f)
+        {
+            if (auto road = traffic_map.get_double_road(0))
+            {// North road
                 road->add_to_forward(std::make_unique<Car>(road->get_forward()));
                 spawned_count++;
             }
