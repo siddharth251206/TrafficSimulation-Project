22574b41d1bff7dab05aa448432f6142f0561687
Pramukh <pramukhkashyaprm@gmail.com>
2025-10-08 17:14:27 +0530

    svnit map with car graphics

--- src/main.cpp
+++ src/main.cpp
@@ -48,7 +48,6 @@ static bool try_load_texture(sf::Texture& tex, const std::string& relPath, const
     }
     return false;
 }
-
 int main()
 {
     constexpr unsigned int width = 1200;
@@ -59,31 +58,81 @@ int main()
     TrafficMap traffic_map;
 
     // Create a four-way intersection
-    const sf::Vector2f center = {600.f, 400.f};
-    const sf::Vector2f north = {600.f, 100.f};
-    const sf::Vector2f south = {600.f, 700.f};
-    const sf::Vector2f east = {1100.f, 400.f};
-    const sf::Vector2f west = {100.f, 400.f};
+    // const sf::Vector2f center = {600.f, 400.f};
+    // const sf::Vector2f north = {600.f, 100.f};
+    // const sf::Vector2f south = {600.f, 700.f};
+    // const sf::Vector2f east = {1100.f, 400.f};
+    // const sf::Vector2f west = {100.f, 400.f};
 
-    traffic_map.add_double_road(north, center); // Road 0
-    traffic_map.add_double_road(south, center); // Road 1
-    traffic_map.add_double_road(west, center);  // Road 2
-    traffic_map.add_double_road(east, center);  // Road 3
+    // traffic_map.add_double_road(north, center); // Road 0
+    // traffic_map.add_double_road(south, center); // Road 1
+    // traffic_map.add_double_road(west, center);  // Road 2
+    // traffic_map.add_double_road(east, center);  // Road 3
 
     // Install traffic lights
-    if (auto intersection = traffic_map.get_junction(center)) 
-        intersection->install_light(sf::seconds(12)); // North -> Center
+    // if (auto intersection = traffic_map.get_junction(center))
+    //     intersection->install_light(sf::seconds(5)); // North -> Center
 
-    // --- Load Car Texture (for sprite-based cars) ---
+    // ------ SVNIT MAP ------
+    const sf::Vector2f gajjar_junction = { 700.f, 100.f };
+    const sf::Vector2f bhabha_junction = { 200.f, 100.f };
+    const sf::Vector2f nehru_junction = { 100.f, 100.f };
+    const sf::Vector2f swami_junction = { 100.f, 200.f };
+    const sf::Vector2f swami_entrance_junction = { 200.f, 200.f };
+    const sf::Vector2f quarters_junction = { 700.f, 375.f };
+    const sf::Vector2f canteen_junction = { 200.f, 375.f };
+    const sf::Vector2f temple_junction = { 900.f, 300.f };
+    const sf::Vector2f gate1_junction = { 200.f, 700.f };
+    const sf::Vector2f gate2_junction = { 700.f, 700.f };
+    const sf::Vector2f library_junction = { 300.f, 375.f };
+    const sf::Vector2f admin_junction = { 300.f, 500.f };
+    const sf::Vector2f department_junction = { 700.f, 500.f };
+
+    traffic_map.add_double_road(gate1_junction, { 200.f, 465.f }, 15.f);
+    traffic_map.add_double_road(bhabha_junction, gajjar_junction, 15.f);
+    traffic_map.add_double_road(nehru_junction, bhabha_junction, 15.f);
+    traffic_map.add_double_road(nehru_junction, swami_junction, 15.f);
+    traffic_map.add_double_road(bhabha_junction, swami_entrance_junction, 15.f);
+    traffic_map.add_double_road(swami_junction, swami_entrance_junction, 15.f);
+    traffic_map.add_double_road(swami_entrance_junction, { 200.f, 300.f }, 15.f);
+    traffic_map.add_double_road({ 200.f, 300.f }, { 700.f, 300.f }, 15.f);
+    traffic_map.add_double_road(gajjar_junction, { 700.f, 300.f }, 15.f);
+
+    traffic_map.add_double_road({ 200.f, 300.f }, canteen_junction, 15.f);
+    traffic_map.add_double_road(canteen_junction, quarters_junction, 15.f);
+    traffic_map.add_double_road({ 700.f, 300.f }, quarters_junction, 15.f);
+
+    traffic_map.add_double_road({ 700.f, 300.f }, temple_junction, 15.f);
+    traffic_map.add_double_road(quarters_junction, { 900.f, 375.f }, 15.f);
+    traffic_map.add_double_road(gajjar_junction, { 900.f, 100.f }, 15.f);
+    traffic_map.add_double_road({ 900.f, 100.f }, temple_junction, 15.f);
+    traffic_map.add_double_road({ 900.f, 375.f }, temple_junction, 15.f);
+
+    traffic_map.add_double_road({ 100.f, 375.f }, canteen_junction, 15.f);
+    traffic_map.add_double_road(canteen_junction, { 200.f, 465.f }, 15.f);
+    traffic_map.add_double_road({ 100.f, 465.f }, { 200.f, 465.f }, 15.f);
+    traffic_map.add_double_road({ 100.f, 465.f }, { 100.f, 375.f }, 15.f);
+    traffic_map.add_double_road({ 100.f, 465.f }, { 100.f, 700.f }, 15.f);
+    traffic_map.add_double_road({ 100.f, 700.f }, gate1_junction, 15.f);
+    traffic_map.add_double_road(gate1_junction, gate2_junction, 15.f);
+
+    traffic_map.add_double_road(library_junction, admin_junction, 15.f);
+    traffic_map.add_double_road(admin_junction, { 300.f, 700.f }, 15.f);
+    traffic_map.add_double_road(department_junction, gate2_junction, 15.f);
+    traffic_map.add_double_road(department_junction, quarters_junction, 15.f);
+    traffic_map.add_double_road(department_junction, admin_junction, 15.f);
+
+    // // --- Load Car Texture (for sprite-based cars) ---
     sf::Texture car_texture;
     bool loaded = false;
     loaded = try_load_texture(car_texture, "assets/premium_car.png", "premium car");
     if (!loaded) loaded = try_load_texture(car_texture, "assets/advanced_car.png", "advanced car");
     if (!loaded) loaded = try_load_texture(car_texture, "assets/car.png", "simple car");
 
+
     // Car Spawner Logic
     sf::Clock spawn_timer;
-    int max_cars = 20;
+    int max_cars = 5;
     int spawned_count = 0;
 
     sf::Clock clock;
@@ -97,16 +146,17 @@ int main()
         }
 
         // Spawn cars on the north and west roads
-        if (spawned_count < max_cars && spawn_timer.getElapsedTime().asSeconds() > 1.5f) {
-            if (auto road = traffic_map.get_double_road(0)) { // North road
-                // Pass texture pointer if loaded, otherwise nullptr and car will fallback to rectangle
+        if (spawned_count < max_cars && spawn_timer.getElapsedTime().asSeconds() > 1.5f)
+        {
+            if (auto road = traffic_map.get_double_road(0))
+            {// North road
                 road->add_to_forward(std::make_unique<Car>(road->get_forward(), loaded ? &car_texture : nullptr));
                 spawned_count++;
             }
-            if (auto road = traffic_map.get_double_road(2)) { // West road
-                road->add_to_forward(std::make_unique<Car>(road->get_forward(), loaded ? &car_texture : nullptr));
-                spawned_count++;
-            }
+            // if (auto road = traffic_map.get_double_road(2)) { // West road
+            //     road->add_to_forward(std::make_unique<Car>(road->get_forward()));
+            //     spawned_count++;
+            // }
             spawn_timer.restart();
         }
 
